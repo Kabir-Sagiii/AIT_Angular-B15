1cc9aeccd0130985c726307007e64c53e80e9bc3
Kabir <sagithedeveloper@gmail.com>
2026-03-26 17:34:53 +0530

    added

---
+++ S05-/src/home/trendingproduct/trendingproduct.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trendingproduct',
+  imports: [],
+  templateUrl: './trendingproduct.html',
+  styleUrl: './trendingproduct.css',
+})
+export class Trendingproduct {
+
+}
