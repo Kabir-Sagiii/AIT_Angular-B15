f4f958b07a9266062cf36b0cdb21630364f76a9f
Kabir <sagithedeveloper@gmail.com>
2026-03-26 17:42:11 +0530

    added

---
+++ S06-/Javascript/demo.js
@@ -0,0 +1,11 @@
+var x = 10;
+
+x = 'hello';
+
+x = true;
+x = 20;
+
+var a = 10;
+var b = '20';
+
+console.log(a + b); // 30  //1020
---
+++ S06-/Javascript/demo.ts
@@ -0,0 +1,14 @@
+
+var x:number = 10
+
+x = "hello"
+
+var num1:number = 10
+
+var num2:number = "20"
+
+console.log(num1+num2)
+
+var y:any = 10
+
+y = "cdjbvdjkfvb"
